f07ec0ede314e4afd264fa98d061524610ab59e7
VitorMChagas <vitormchagas@gmail.com>
2021-05-24 03:52:13 -0300

    Adding Menu Elements

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # image-shrink
  Electron app to shrink image size
+
+# SUID sandbox Error
+Uninstall electron npm uninstall electron
+Change npm config platform export npm_config_platform=win32
+Install electron npm install electron
+Unset the environment variable unset npm_config_platform
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 // Set env
 process.env.NODE_ENV = 'development'
@@ -15,24 +15,90 @@ function createMainWindow () {
         height: 600,
         icon:  `${__dirname}/assets/icons/Icon_256x256.png`,
         resizable: isDev,
+        backgroundColor: 'white',
     })
 
     mainWindow.loadURL(`file://${__dirname}/app/index.html`)
     // mainWindow.loadFile('./app/index.html')
 }
 
-app.on('ready', createMainWindow)
+function createAboutWindow () {
+    aboutWindow = new BrowserWindow({
+        title: 'About ImageShrink',
+        width: 300,
+        height: 300,
+        icon:  './assets/icons/Icon_256x256.png',
+        resizable: false,
+        backgroundColor: 'white',
+    })
+
+    aboutWindow.loadURL(`file://${__dirname}/app/about.html`)
+    // mainWindow.loadFile('./app/index.html')
+}
+
+app.on('ready', () => {
+    createMainWindow()
+
+    const mainMenu = Menu.buildFromTemplate(menu)
+   
+    Menu.setApplicationMenu(mainMenu)
+
+    mainWindow.on('closed', () => mainWindow = null)
+})
+
+const menu = [
+    ...(isMac ? [
+        {
+            label: app.name,
+            submenu: [
+                {
+                    label: 'About',
+                    click: createAboutWindow,
+                },
+            ],
+        },
+    ]: []),
+    {
+        role: 'fileMenu',
+    },
+    
+    ...(!isMac ? [
+        {
+            label: 'Help',
+            submenu: [
+                {
+                    label: 'About',
+                    click: createAboutWindow,
+                },
+            ],
+        },
+    ]: []),
+
+    ...(isDev 
+        ? [
+            {
+                label: 'Developer',
+                submenu: [
+                    { role: 'reload' },
+                    { role: 'forcereload' },
+                    { type: 'separator' },
+                    { role: 'toggledevtools' },
+                ],
+            },
+        ]
+    : []),
+];
 
 app.on('window-all-closed', () => {
     if (!isMac) {
         app.quit()
     }
-})
+});
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createMainWindow()
     }
-})
+});
 
 app.allowRendererProcessReuse = true
